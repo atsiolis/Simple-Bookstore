73b6126c1200d3b273101814865674c887e8366c
atsiolis <tsiolis26@gmail.com>
2022-11-12 01:40:40 +0200

    Starting project

--- project_lib.c
+++ project_lib.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "project_lib.h"
 
 int menu()
@@ -18,3 +19,4 @@ int menu()
 
     return choice;
 }
+
--- project_lib.h
+++ project_lib.h
@@ -1,1 +1,27 @@
+// #if project_lib_h
+// #define project_list.h
+
+typedef struct book
+{
+    char *title;
+    int release_date;
+    float price;
+}book_t;
+
+typedef struct author
+{
+    int writer_id;
+    char *surname;
+    char *name;
+    int num_of_books;
+}author_t;
+
+typedef struct writes
+{
+    char *title;
+    int writer_id;
+}writes_t;
+
 int menu();
+
+// #endif
