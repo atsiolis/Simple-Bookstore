6a91d2fdffe8a874a1f0d6147686cd6dd25024b7
atsiolis <tsiolis26@gmail.com>
2022-11-11 22:16:59 +0200

    created .h file

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "project_lib.h"
 
-int menu();
 
 int main(int argc, char *argv[])
 {
@@ -9,19 +9,3 @@ int main(int argc, char *argv[])
     choice = menu();
 }
 
-int menu()
-{
-    int choice;
-
-    printf("Choose an operation:\n");
-    printf("1. Insert new writer record.\n");
-    printf("2. Insert new book record.\n");
-    printf("3. Search a writer record.\n");
-    printf("4. Search a book record. \n");
-    printf("5. Delete a writer record.\n");
-    printf("6. Delete a book record. \n");
-    printf("7. Exit\n");
-    scanf("%d", &choice);
-
-    return choice;
-}
