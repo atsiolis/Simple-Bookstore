f960a70359aa347fc2097a7e6b5110cba02ec5c6
abilioni <bilionkata@gmail.com>
2022-11-11 21:53:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+int menu();
+
+int main(int argc, char *argv[])
 {
-    printf("Hello, world!");
+    int choice;
+    choice = menu();
+}
+
+int menu()
+{
+    int choice;
+
+    printf("Choose an operation:\n");
+    printf("1. Insert new writer record.\n");
+    printf("2. Insert new book record.\n");
+    printf("3. Search a writer record.\n");
+    printf("4. Search a book record. \n");
+    printf("5. Delete a writer record.\n");
+    printf("6. Delete a book record. \n");
+    printf("7. Exit\n");
+    scanf("%d", &choice);
+
+    return choice;
 }
